64c98b8c0612b170a148a6c4bbfa4203e04652bc
Anshul Yadav <anshul333y@gmail.com>
2026-04-05 19:03:41 +0530

    added plugin neopywal and vim-tmux-navigator

---
+++ lua/plugins/neopywal.lua
@@ -0,0 +1,10 @@
+return {
+  -- "RedsXDD/neopywal.nvim",
+  -- lazy = false,
+  -- priority = 1000,
+  -- opts = {},
+  -- config = function(_, opts)
+  --   require("neopywal").setup(opts)
+  --   vim.cmd.colorscheme("neopywal")
+  -- end,
+}
